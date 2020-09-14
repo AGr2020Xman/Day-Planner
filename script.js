80503efecefeafe64ccf22d986d779b09f55f3b8
Andre Grech <silver.grech@gmail.com>
2020-09-14 17:01:14 +1000

    first commit

---
+++ script.js
@@ -0,0 +1,50 @@
+var previousEntries = [{ () }];
+
+var timeblocks = $("#timeblocks");
+
+// incase i need to initialise moment.js
+var momentTime = () => {
+
+};
+
+var 
+
+// to local storage
+var saveEntry = () => {
+
+};
+
+// and edit local storage
+var editEntry = () => {
+
+};
+
+// clear local storage + current entries + warning of Are you sure? You can not get these back.
+var clearAllEntries = () => {
+    
+};
+
+var createTimeSlots = () => {
+    var createTimeslots = $("<section class=entry-boxes id=entryBox></section>")
+
+};
+
+// do not allow tampering with times in the past
+var lockPreviousTimeSlots = () => {
+
+};
+
+// allow access to COLOUR CODED future time slots
+var openAvailableTimeSlots = () => {
+
+};
+
+// highlight (maybe slight hover + colour change) or currently active hour
+var highlightPresentTimeSlot = () => {
+
+};
+
+// retrive local storage
+var getSavedEntries = () => {
+
+};

940b9bf05231660baae53ae452fe52fc2d8e0964
Andre Grech <silver.grech@gmail.com>
2020-09-16 15:13:16 +1000

    setting up local storage pull + creating elements with the retrieved data

--- script.js
+++ script.js
@@ -11,14 +11,25 @@ var currentDateTime = () => {
 };
 
 
+// open dayplanner - top of page - Current Date/Year
+// day planner organised in 1 hour blocks starting @ 9am - ending @ 5pm
+// Times in the past, will be grayed out + normal size
+// Present time will be slightly hovering + different colour
+// future times will be green and normal sized
 
-
+// I want to create 8 1 hour time blocks (dynamically probably)/or maybe preset - with dynamic class changing LINKED to the moment
+var time = moment().format()("h:mm:ss");
 
 var 
 
 // to local storage
-var saveEntry = () => {
-
+var saveEntry = (time, input) => {
+    // unhide you have saved your entry 
+    savedPlanEntries.push({
+        "time":time,
+        "event":input
+    });
+    localStorage.setItem("savedPlanEntries",JSON.stringify(savedPlanEntries));
 };
 
 // and edit local storage
@@ -31,8 +42,41 @@ var clearAllEntries = () => {
     
 };
 
+let savedPlanEntries;
+let scheduleArray = [];
+
+var populateSavedEntries = () =>{
+    getSavedEntries();
+    createSavedEntries();
+}
+// retrive local storage
+var getSavedEntries = () => {
+    scheduleArray = [];
+    // value of var set
+    savedPlanEntries = localStorage.getItem("savedPlanEntries");
+    if (savedPlanEntries === null || savedPlanEntries === ""){
+        scheduleArray = [];
+    } else {
+        savedPlanEntries = JSON.parse(savedPlanEntries); 
+        
+        for (i=0; i<savedPlanEntries.length; i++) {
+            scheduleArray.push(savedPlanEntries[i].time); 
+        }
+    }
+};
+
+var createSavedEntries = () => {
+    for (i = 0;i<scheduleArray.length;i++){
+        var scheduleBlockElementID = "#" + scheduleArray[i];
+        var scheduleBlockElement = $(scheduleBlockElementID).children(".row").children("text-area");
+        $(scheduleBlockElementID).children('.row').children('button').attr("data-event", "true");
+        scheduleBlockElement.val(savedPlanEntries[i].event)
+    }
+};
+
 var createTimeSlots = () => {
-    var createTimeslots = $("<section class=entry-boxes id=entryBox></section>")
+    // var createTimeslots = $("<section class=entry-boxes id=entryBox></section>")
+
 
 };
 
@@ -52,7 +96,3 @@ var highlightPresentTimeSlot = () => {
 
 };
 
-// retrive local storage
-var getSavedEntries = () => {
-
-};
